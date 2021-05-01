e74fd627c33985177ec3500d3c65f131f85ab71a
spnraju <padmanabharaju.sagi@gmail.com>
2021-05-01 12:16:20 +0530

    fix: replace failing mercury with the internet app

---
+++ tests/internet-app.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tags: ['internet'],
+  'Internet App': function (browser) {
+    browser
+      .url('http://the-internet.herokuapp.com/')
+      .maximizeWindow()
+      .waitForElementVisible('body', 3000)
+      .assert.title('The Internet')
+      .pause(5000)
+      .end();
+  }
+};
