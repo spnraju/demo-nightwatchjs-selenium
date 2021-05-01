bef7fe3a5add94cbdf326a7a8bcdc9f708b5e17d
spnraju <padmanabharaju.sagi@gmail.com>
2021-05-01 12:16:20 +0530

    fix: replace failing mercury with the internet app

--- tests/google.js
+++ tests/google.js
@@ -2,13 +2,12 @@ module.exports = {
   tags: ['google'],
   'Demo test Google': function (client) {
     client
-      .url('http://google.co.in')
+      .url('http://google.com')
       .pause(1000);
 
     client.expect.element('body').to.be.present;
-
-    client.expect.element('body').to.have.attribute('class').which.contains('vasq');
-    client.expect.element('body').to.have.attribute('class').which.matches(/vasq$/);
+    // client.expect.element('body').to.have.attribute('class').which.contains('EM1Mrb');
+    // client.expect.element('body').to.have.attribute('class').which.matches(/EM1Mrb$/);
     client.expect.element('body').to.have.attribute('class').before(1000);
 
     client.end();
