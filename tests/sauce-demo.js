0c20c5922988636285963165bf236ff156e220f3
spnraju <padmanabharaju.sagi@gmail.com>
2022-05-23 00:20:00 +0530

    fix: replace google test with sauce demo

---
+++ tests/sauce-demo.js
@@ -0,0 +1,15 @@
+module.exports = {
+  tags: ['sauce-demo'],
+  'Demo test Sauce demo': function (client) {
+    client
+      .url('https://www.saucedemo.com/')
+      .waitForElementVisible('body', 1000)
+      .getTitle(function (result) {
+        client.assert.equal(result.indexOf('Swag Labs'), 0, 'Page title starts with nightwatch')
+      });
+  },
+
+  after: function (client) {
+    client.end();
+  }
+};
